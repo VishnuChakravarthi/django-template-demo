d640f6ae9a7947561f6fc330de53aecd1ac7d37f
VishnuChakravarthi <vishchakravarthy18@gmail.com>
2022-08-04 09:35:25 +0530

    added template filters

--- template_app/views.py
+++ template_app/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'template_app/index.html')
+    context_dict = {'text': "Hello World"}
+    return render(request, 'template_app/index.html', context=context_dict)
 
 
 def second_page(request):
